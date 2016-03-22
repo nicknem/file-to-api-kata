9e9b85a7260bd2f543bcbc8e7ecf4780668c233c
Romain Champourlier <pro@rchampourlier.com>
2016-03-22 22:44:29 +0100

    Implemented basic POST with update of resources

--- app.rb
+++ app.rb
@@ -2,16 +2,54 @@ require 'rack'
 require 'rack/server'
 require 'pry'
 require 'json'
+require 'cgi/core'
 
 class MyApp
+  FILE = 'data.json'
+
   def response(env)
-    resource = env['PATH_INFO'][1..-1] #remove first character which is a /
-    json = JSON.parse(File.read('data.json'))[resource].to_json
-    [200, {}, [json]]
+    verb = env['REQUEST_METHOD']
+    response = (
+      case verb
+      when 'GET' then get(env)
+      when 'POST' then post(env)
+      else fail 'Not implemented'
+      end
+    )
+    [200, {}, [response]]
   end
+
+  def get(env)
+    parsed_file[resource(env)].to_json
+  end
+
+  def resource(env)
+    env['PATH_INFO'][1..-1] # remove first character which is a /
+  end
+
+  def post(env)
+    input = env['rack.input'].read
+    new_movie = CGI.parse(input)
+    new_movie.each { |k, v| new_movie[k] = v.first }
+    new_content = parsed_file.dup
+    new_content[resource(env)].push(new_movie)
+    write_file(new_content)
+    'ok'
+  end
+
+  def parsed_file
+    JSON.parse(File.read(FILE))
+  end
+
+  def write_file(content)
+    File.write(FILE, content.to_json)
+  end
+
   def self.call(env)
     MyApp.new.response(env)
   end
 end
 
-Rack::Server.start :app => MyApp
+unless ENV['RACK_ENV'] == 'test'
+  Rack::Server.start :app => MyApp
+end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,28 +1,75 @@
 require 'pry'
 require 'json'
-require "rack/test"
+require 'rack/test'
+
+ENV['RACK_ENV'] = 'test'
 require_relative '../app'
+
 include Rack::Test::Methods
 
 def app
   MyApp
 end
 
+def parsed_body
+  body = last_response.body
+  JSON.parse(body)
+end
+
 # First test to see that it works
 describe 'my_app' do
-  describe 'get/movies' do
+
+  after(:each) do
+    FileUtils.copy('data.json.orig', 'data.json')
+  end
+
+  describe 'GET /movies' do
     it 'should return 6 movies' do
       get '/movies'
-      body = last_response.body
-      expect(JSON.parse(body).count).to eq(6)
-    end
-  end
-  describe 'get/directors' do
-    it 'should return 3 directors' do
-      get '/directors'
-      body = last_response.body
-      expect(JSON.parse(body).count).to eq(3)
+      expect(parsed_body.count).to eq(6)
     end
   end
 
+  describe 'GET /directors' do
+    it 'should return 3 directors' do
+      get '/directors'
+      expect(parsed_body.count).to eq(3)
+    end
+  end
+
+  describe 'POST /movies' do
+    let(:movie) { { title: 'My Super Movie', year: 2016, directory_id: 1 } }
+
+    it 'is successful' do
+      post '/movies', movie
+      last_response.ok?
+    end
+
+    it 'adds a movie' do
+      post '/movies', movie
+      get '/movies'
+      expect(parsed_body.count).to eq(7)
+    end
+  end
+
+  describe 'POST /directors' do
+    let(:director) { { 'name' => 'David Me' } }
+
+    it 'is successful' do
+      post '/directors', director
+      last_response.ok?
+    end
+
+    it 'adds a director' do
+      post '/directors', director
+      get '/directors'
+      expect(parsed_body.count).to eq(4)
+    end
+
+    it 'adds the correct director' do
+      post '/directors', director
+      get '/directors'
+      expect(parsed_body.last).to eq(director)
+    end
+  end
 end
